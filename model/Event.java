6d1386ea5a42dd64842d171dbe291510ffd2ea0b
tsabitasyaa <tsasya16@gmail.com>
2025-06-09 14:29:38 +0700

    Update Event.java

--- model/Event.java
+++ model/Event.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
-
-/**
- *
- * @author tsabi
- */
 
 import java.time.LocalDate;
 import java.time.LocalTime;
