a7f5ff7fc1c1f7ea470edb8262a21ca4f115cef5
tsabitasyaa <tsasya16@gmail.com>
2025-06-09 14:31:25 +0700

    Update Team.java

--- model/Team.java
+++ model/Team.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author tsabi
- */
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Team {
     private final String id;
@@ -35,6 +24,5 @@ public class Team {
     public void addMember(String userId) {
         if (!memberIds.contains(userId)) memberIds.add(userId);
     }
-
 
 }
